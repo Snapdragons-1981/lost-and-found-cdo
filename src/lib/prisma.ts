c21c2343bb71e4558aae2351212e1fdc0a2e0f17
unknown <ganaswenrick90@gmail.com>
2026-08-05 02:59:47 +0800

    Downgrade to Prisma 5 - fixes Turbopack compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,22 +1,10 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const { PrismaClient } = require(".prisma/client/default");
-const { PrismaPg } = require("@prisma/adapter-pg");
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    throw new Error("DATABASE_URL is not set");
-  }
-
-  const adapter = new PrismaPg({ connectionString });
-  return new PrismaClient({ adapter });
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
